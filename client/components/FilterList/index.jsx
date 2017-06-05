717f3b903eef2eeb9c95aff7b5b6797d2134092b
Shawn Stern <stern.shawn@gmail.com>
2017-06-05 14:54:03 -0700

    Updating filter list carousel to auto-scroll to new selection

--- client/components/FilterList/index.jsx
+++ client/components/FilterList/index.jsx
@@ -13,6 +13,7 @@ class FilterList extends Component {
   }
 
   componentDidMount() {
+    // Initialize Flickity on mount and save a reference to it
     const flickityInstance = new Flickity(this.filterList, {
       cellAlign: 'center',
       contain: true,
@@ -27,34 +28,42 @@ class FilterList extends Component {
     });
   }
 
-  render() {
+  // When we select a filter, enable it and tell Flickity to scroll to that filter
+  setFilter = (index) => {
+    this.props.selectFilter(index);
+    this.state.flickity.select(index);
+  }
+
+  render = () => {
     const {
       filters,
       photo,
-      selectFilter,
       selectedFilter,
     } = this.props;
 
+    // Return a wrapper div containing a generated list of preview tiles for each filter option,
+    // the selected filter will have conditional styling applied to show emphasis
     return (
       <div className="pa3" ref={(el) => { this.filterList = el; }}>
         {filters.map((filter, index) => {
           const filterClass = classNames(
-            'tc', 'dib', 'mr3', `${styles.filter}`,
+            'tc',
+            'dib',
+            'mr3',
+            `${styles.filter}`,
             {
               [styles['is-selected']]: index === selectedFilter,
             }
           );
 
           return (
-            <div className={`tc dib mr3 ${styles.filter}`} key={index} onClick={() => selectFilter(index)}>
+            <div className={`tc dib mr3 ${styles.filter}`} key={index} onClick={() => this.setFilter(index)}>
               <h4 className={filterClass}>{filter.displayName}</h4>
               <div className={`${filter.className} ${styles.filterPreview} aspect-ratio aspect-ratio--1x1`}>
                 <div className="aspect-ratio--object cover" style={{ backgroundImage: `url(${photo})` }} />
               </div>
             </div>);
-          }
-          )
-        }
+        })}
       </div>
     );
   }
